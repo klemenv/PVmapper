aaad01f924909671f6aa58f9f3c07f01a3df2c12
Klemen Vodopivec <klemen@vodopivec.org>
2025-07-06 22:10:29 -0400

    Combine replies from the same IOC and to the same client into one transmition

--- src/proto.hpp
+++ src/proto.hpp
@@ -12,6 +12,7 @@ class Protocol {
         virtual std::pair<Bytes, uint16_t> createSearchRequest(const std::vector<std::pair<uint32_t, std::string>>& pvs) = 0;
         virtual bool updateSearchReply(Bytes& reply, uint32_t chanId) = 0;
         virtual bool updateSearchReply(Bytes& reply, const std::string& iocIp, uint16_t iocPort) = 0;
+        virtual std::vector<Bytes> combineSearchReplies(const std::vector<Bytes>& replies) = 0;
         virtual std::vector<std::pair<uint32_t, std::string>> parseSearchRequest(const Bytes& buffer) = 0;
         virtual std::vector<std::pair<uint32_t, Bytes>> parseSearchResponse(const Bytes& buffer) = 0;
         virtual std::pair<std::string, uint16_t> parseIocAddr(const std::string& ip, uint16_t udpPort, const Bytes& buffer) = 0;
--- src/proto_ca.hpp
+++ src/proto_ca.hpp
@@ -8,6 +8,7 @@ class ChannelAccess : public Protocol {
         std::pair< Bytes, uint16_t> createSearchRequest(const std::vector<std::pair<uint32_t, std::string>> &pvs);
         bool updateSearchReply(Bytes& reply, uint32_t chanId);
         bool updateSearchReply(Bytes& reply, const std::string& iocIp, uint16_t iocPort);
+        std::vector<Bytes> combineSearchReplies(const std::vector<Bytes>& replies);
         std::vector<std::pair<uint32_t, std::string>> parseSearchRequest(const Bytes &buffer);
         std::vector<std::pair<uint32_t, Bytes>> parseSearchResponse(const Bytes& buffer);
         std::pair<std::string, uint16_t> parseIocAddr(const std::string& ip, uint16_t udpPort, const Bytes& buffer);
